02a6e2afc39ec1cc2ea8e4434c5c049e51ad2ef1
Alan Li <li.alan011@gmail.com>
2020-06-16 22:05:55 -0700

    add auth0 react components for set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import PrivateRoute from './components/PrivateRoute';
+
+import { Router, Route, Switch } from 'react-router-dom';
+import Profile from './components/Profile';
+import history from './utils/history';
 
 function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+	return (
+		<div className="App">
+			{/* Don't forget to include the history module */}
+			<Router history={history}>
+				<header>
+					<NavBar />
+				</header>
+				<Switch>
+					<Route path="/" exact />
+					<PrivateRoute path="/profile" component={Profile} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
