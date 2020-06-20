c34b880ef716127690c752b25f4bccfb310a8230
Alan Li <li.alan011@gmail.com>
2020-06-20 16:36:32 -0700

    import Box for material ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Tab, Tabs, Typography } from '@material-ui/core';
+import { Tab, Tabs, Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
