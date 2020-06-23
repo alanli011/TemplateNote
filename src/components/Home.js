95ee6f48c32853e8edd347c60c4bf348aa9c1375
Alan Li <li.alan011@gmail.com>
2020-06-23 15:25:01 -0700

    create navigation component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navigation from './Navigation';
+// import Navigation from './Navigation';
 
 import { useAuth0 } from '../react-auth0-spa';
 
 const Home = (props) => {
 	const { isAuthenticated } = useAuth0();
 
-	return <div>{isAuthenticated && <Navigation />}</div>;
+	return <div>{isAuthenticated && <h1>Welcome Home</h1>}</div>;
 };
 
 export default Home;
