b109d636691fc81e765ed6d49bfb003b6f780b2f
Alan Li <li.alan011@gmail.com>
2020-06-24 22:37:36 -0700

    add reactquill to notes component. encountering a type of bug because the id needs to be set in the DOM?

--- src/components/notebooks/Notes.js
+++ src/components/notebooks/Notes.js
@@ -1,15 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import PropTypes from 'prop-types';
+
 import SingleNoteBook from './SingleNotebook';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1
+	},
+	maxWidth: {
+		width: '100%'
+	}
+}));
+
 const Notes = (props) => {
+	const [ value, setValue ] = useState('');
+	const classes = useStyles();
+
+	const modules = {
+		toolbar: [
+			[ { font: [] } ],
+			[ { size: [ 'small', false, 'large', 'huge' ] } ],
+			[ 'bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block' ],
+			[ { list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' } ],
+			[ { color: [] }, { background: [] } ],
+			[ 'link', 'image', 'video' ],
+			[ 'clean' ]
+		]
+	};
+
+	const formats = [
+		'font',
+		'size',
+		'bold',
+		'italic',
+		'underline',
+		'strike',
+		'blockquote',
+		'code-block',
+		'list',
+		'bullet',
+		'indent',
+		'color',
+		'background',
+		'link',
+		'image',
+		'video'
+	];
+
 	return (
 		<React.Fragment>
 			<div>
 				<SingleNoteBook />
 			</div>
-			<h1>My notes</h1>
+			<ReactQuill
+				id={props.id ? props.id : ''}
+				theme="snow"
+				value={value}
+				onChange={setValue}
+				className={classes.maxWidth}
+				modules={modules}
+				formats={formats}
+			/>
 		</React.Fragment>
 	);
+};
+
+Notes.propTypes = {
+	id: PropTypes.string.isRequired
 };
 
 export default Notes;
