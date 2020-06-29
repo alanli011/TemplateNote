6d023469d711ce475995ffdc92d6d22c8a5bab6b
Alan Li <li.alan011@gmail.com>
2020-06-28 23:34:47 -0700

    remove trailing / from production config file

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const baseUrl = {
 	url:
 		process.env.NODE_ENV === 'development'
 			? process.env.REACT_APP_DEV_API || 'http://localhost:5000'
-			: process.env.REACT_APP_PROD_API || 'https://templatenote-backend.herokuapp.com/'
+			: process.env.REACT_APP_PROD_API || 'https://templatenote-backend.herokuapp.com'
 };
 
 export default baseUrl;
